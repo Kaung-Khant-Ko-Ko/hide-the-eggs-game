3a7ef695d7c117c6ae030c3b88464974c59494a3
Kaung Khant Ko Ko <59255928+Kaung-Khant-Ko-Ko@users.noreply.github.com>
2024-06-05 20:04:14 +0630

    Add files via upload

--- assets/scripts/play.js
+++ assets/scripts/play.js
@@ -4,6 +4,7 @@ let player_score = 0;
 let comp_score = 0;
 let turn = 0;
 let rand_select = null;
+let selected_areas = [];
 
 const messages = $(".message")
   .map(function () {
@@ -42,7 +43,9 @@ for (let i = eggs; i > 0; i--) {
   messages[i - 1].textContent = "🥚";
 }
 
-$("#count").text(eggs + " eggs");
+$(".field__area").css("font-size", $(".field__area").width() / 2);
+
+$(".count-message").text(`You have ${eggs} eggs left to hide`);
 
 $("#result__player").text(player_score + " egg");
 $("#result__comp").text(comp_score + " egg");
@@ -67,9 +70,9 @@ $(".field__area").click(function () {
   if (eggs === 0) {
     $(".count-message").text("You have hid all the eggs");
   } else if (eggs > 1) {
-    $("#count").text(eggs + " eggs");
+    $(".count-message").text(`You have ${eggs} eggs left to hide`);
   } else {
-    $("#count").text(eggs + " egg");
+    $(".count-message").text(`You have ${eggs} egg left to hide`);
   }
 });
 
@@ -85,6 +88,7 @@ $("#ready-btn").click(function () {
     }
     $(".header__text").text("Search the eggs on the field on the right");
     $(".main__hiding-state").addClass("close-hide-state");
+    $(".main__play-state").css("padding", "0px 50px")
     $(".main__play-state").animate({ width: "100%" });
     for (let m = 0; m < p_areas.length; m++) {
       if (hide_areas.includes("area_" + m)) {
@@ -95,16 +99,22 @@ $("#ready-btn").click(function () {
 });
 
 $(".comp__area").click(function () {
+  selected_areas.push(this.id);
   if (comp_eggs.includes(this.id)) {
     this.textContent = "🥚";
     player_score++;
   }
   $(this).css("background-image", "url('../assets/images/Ground.jpg')");
   this.setAttribute("disabled", "");
+
   if (player_score > 1) {
     $("#result__player").text(player_score + " eggs");
   } else {
     $("#result__player").text(player_score + " egg");
+  }
+
+  for (let n = 0; n < c_areas.length; n++) {
+    c_areas[n].setAttribute("disabled", "");
   }
 
   setTimeout(() => {
@@ -122,5 +132,31 @@ $(".comp__area").click(function () {
       $("#result__comp").text(comp_score + " egg");
     }
     turn++;
+
+    setTimeout(() => {
+      if (player_score === 3 || comp_score === 3) {
+        if (player_score === 3) {
+          window.location.assign("result_win.html");
+        } else {
+          window.location.assign("result_lose.html");
+        }
+      }
+    }, 500);
+
+    for (let o = 0; o < c_areas.length; o++) {
+      if (selected_areas.includes(c_areas[o].id) === false) {
+        c_areas[o].removeAttribute("disabled");
+      }
+    }
   }, 1000);
+
+  setTimeout(() => {
+    if (player_score === 3 || comp_score === 3) {
+      if (player_score === 3) {
+        window.location.assign("result_win.html");
+      } else {
+        window.location.assign("result_lose.html");
+      }
+    }
+  }, 500);
 });
